840ff467102f6ce2b2091f7389831454e6845797
hmy1000k <hmy1000K@gmail.com>
2022-12-07 00:03:06 +0800

    新增留言页面

--- src/homepage/main.js
+++ src/homepage/main.js
@@ -1,37 +1,11 @@
 import Vue from 'vue'
 import router from './router.js'
 import homepage from './index.vue'
+import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-import {
-  Container,
-  Header,
-  Main,
-  Footer,
-  Row,
-  Col,
-  Tabs,
-  TabPane,
-  Carousel,
-  CarouselItem,
-  Image,
-  Divider,
-  Pagination
-} from 'element-ui'
 Vue.config.productionTip = false
 
-Vue.use(Container)
-Vue.use(Header)
-Vue.use(Main)
-Vue.use(Footer)
-Vue.use(Row)
-Vue.use(Col)
-Vue.use(Tabs)
-Vue.use(TabPane)
-Vue.use(Divider)
-Vue.use(Image)
-Vue.use(Carousel)
-Vue.use(CarouselItem)
-Vue.use(Pagination)
+Vue.use(ElementUI)
 
 new Vue({
   router,
--- src/homepage/views/leaveWord/index.vue
+++ src/homepage/views/leaveWord/index.vue
@@ -1,23 +1,266 @@
 <template>
   <div class="leave-word-content">
-    留言
-  </div>
+    <div class="header-bg">
+      <div class="title">与首长对话，心系报国梦</div>
+    </div>
+    <div class="message-class">
+      <el-tabs v-model="messageClass">
+        <el-tab-pane label="留言类型1" name="messageClass1" />
+        <el-tab-pane label="留言类型2" name="messageClass2" />
+      </el-tabs>
+      <div class="search">
+        <el-input v-model="searchVuale" style="width: 300px;margin-right: 10px;" placeholder="请输入内容" />
+        <el-button icon="el-icon-search" type="primary" circle />
+      </div>
+    </div>
+    <div class="message-main">
+      <div class="message-box">
+        <el-input v-model="messageContent" type="textarea" :rows="3" placeholder="您想说点什么？" resize="none" />
+        <el-button v-show="(messageContent.length > 0)" type="primary" style="margin-top: 10px;"> 发表留言 </el-button>
+      </div>
+      <div class="message-list">
+        <div v-for="msg in messageList.rows" :key="msg.serialNumber" class="message-item">
+          <div class="top">
+            <p class="serial-number">NO.{{ msg.serialNumber }}</p>
+            <p>匿名</p>
+          </div>
+          <div class="message-content">
+            <p>{{ msg.content }}</p>
+            <div v-if="(msg.subitem.length > 0)" class="subitem-list">
+              <div v-for="(subitem, i) in msg.subitem" :key="subitem.serialNumber" class="subitem-item">
+                <span style="color:#949494 ;">{{ (i + 1) }}.匿名回复: </span>
+                <p class="subitem-content">{{ subitem.content
+                }}
+                </p>
+                <p class="subitem-time">{{ subitem.time }}</p>
+              </div>
+            </div>
+          </div>
+          <div v-if="openMessagesNO === msg.serialNumber" class="message-tool">
+            <el-input v-model="msg.comment" class="message-tool-input" type="textarea" :rows="2" placeholder="您想说点什么？"
+              resize="none" />
+            <div class="message-tool-button">
+              <el-button type="warning" @click="messagesSubmit(msg)">提交</el-button>
+              <el-button @click="openMessagesNO = ''">取消</el-button>
+            </div>
+          </div>
+          <div class="bottom">
+            <p class="time">{{ msg.time }}</p>
+            <el-button type="text" icon="el-icon-chat-line-round" @click="openMsg(msg)">评论</el-button>
+          </div>
 
+        </div>
+      </div>
+
+    </div>
+  </div>
 </template>
 <script>
+const messages = [
+  {
+    serialNumber: '123123',
+    content: '内容1',
+    time: '2022-10-26 20:16:12',
+    subitem: [
+      {
+        serialNumber: '123123',
+        content: '回复内容1',
+        time: '2022-10-26 20:16:12'
+      }
+    ],
+    comment: ''
+  },
+  {
+    serialNumber: '123124',
+    content: '内容2',
+    time: '2022-10-26 20:16:12',
+    subitem: [
+      {
+        serialNumber: '123123',
+        content: '回复内容2',
+        time: '2022-10-26 20:16:12'
+      }
+    ],
+    comment: ''
+  },
+  {
+    serialNumber: '123125',
+    content: '内容3',
+    time: '2022-10-26 20:16:12',
+    subitem: [
+      {
+        serialNumber: '123123',
+        content: '回复内容3',
+        time: '2022-10-26 20:16:12'
+      }
+    ],
+    comment: ''
+  },
+  {
+    serialNumber: '123126',
+    content: '内容4',
+    time: '2022-10-26 20:16:12',
+    subitem: [
+      {
+        serialNumber: '123123',
+        content: '回复内容4',
+        time: '2022-10-26 20:16:12'
+      }
+    ],
+    comment: ''
+  }
+]
 export default {
   name: 'LeaveWord',
   data() {
     return {
-
+      messageList: {
+        rows: messages,
+        paging: {
+          pageSize: 20,
+          page: 1,
+          total: 100
+        }
+      },
+      openMessagesNO: '',
+      messageContent: '',
+      messageClass: 'messageClass1',
+      searchVuale: ''
     }
   },
   methods: {
+    openMsg(msg) {
+      msg.comment = ''
+      this.openMessagesNO = msg.serialNumber
+    },
+    messagesSubmit(msg) {
+      const comment = msg.comment
+      // 添加评论请求
 
+      // 成功后
+      msg.subitem.push({
+        serialNumber: '123123',
+        content: comment,
+        time: '2022-10-26 20:16:12'
+      })
+      msg.comment = 0
+      this.openMessagesNO = ''
+    }
   }
 }
 </script>
 
 <style lang="scss">
-.leave-word-content {}
+$sideSistance: 160px;
+
+.leave-word-content {
+  .header-bg {
+    height: 120px;
+    background-color: #fff;
+
+    .title {
+      text-align: center;
+      padding-top: 20px;
+      font-size: 25px;
+    }
+  }
+
+  .message-class {
+    padding: 10px $sideSistance 0px $sideSistance;
+    display: flex;
+    justify-content: space-between;
+
+    .el-tabs__item.is-active {
+      color: #fcb955 !important;
+    }
+
+    .el-tabs__active-bar {
+      background-color: #fcb955 !important;
+    }
+
+    .search {}
+  }
+
+  .message-main {
+    padding: 10px $sideSistance 0px $sideSistance;
+    background-color: #f4f4f4;
+
+    .message-box {
+      text-align: right;
+    }
+
+    .message-list {
+      padding: 10px 0 0 0;
+
+      .message-item {
+        background-color: #fff;
+        border-radius: 5px;
+        margin-bottom: 10px;
+        padding: 10px 20px;
+
+        .top {
+          display: flex;
+          padding-bottom: 7px;
+          border-bottom: #f4f4f4 2px solid;
+
+          .serial-number {
+            font-weight: 600;
+            width: 200px;
+          }
+        }
+
+        .message-content {
+          padding: 10px 30px;
+
+          .subitem-list {
+            padding: 20px 30px 0 30px;
+
+            .subitem-item {
+              display: flex;
+              justify-content: space-between;
+
+              .subitem-content {
+                flex: 1;
+              }
+
+              .subitem-time {
+                text-align: right;
+                color: #949494;
+                width: 200px;
+              }
+            }
+          }
+        }
+
+        .message-tool {
+          padding: 0 30px 0 60px;
+          display: flex;
+          align-items: flex-end;
+
+          .message-tool-input {
+            flex: 1;
+          }
+
+          .message-tool-button {
+            width: 200px;
+            padding: 0 10px;
+          }
+        }
+
+        .bottom {
+          display: flex;
+          align-items: center;
+
+          .time {
+            color: #949494;
+            width: 300px;
+          }
+
+        }
+
+      }
+
+    }
+  }
+}
 </style>
